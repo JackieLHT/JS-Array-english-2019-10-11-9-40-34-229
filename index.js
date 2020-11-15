52eb4e55b48db917c555488c1431a885a6f252cd
JACKIE LEUNG (MIS-ISD-OOCLL/HKG) <LEUNGJA3@OOCL.COM>
2020-11-16 00:10:53 +0800

    completed

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+// a is of type string and b is of type array.
 
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+a = a.map(x => x*2);
+console.log(a);
 
 
 // Write the program, according to the following requirements output results.
@@ -14,13 +17,38 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
-
+var case1 = colors.join(' ');
+var case2 = colors.join('+');
+var case3 = colors.join(',');
+console.log(case1);
+console.log(case2);
+console.log(case3);
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+a = a.sort(function(a, b){return b - a});
+console.log(a);
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var max_freq = 1; 
+var count = 0;
+var max_element;
+for (var i=0; i<a.length; i++)
+{
+        for (var j=i; j<a.length; j++)
+        {
+                if (a[i] == a[j])
+                 count++;
+                if (max_freq<count)
+                {
+                  max_freq=count;
+                  max_element = a[i];
+                }
+        }
+        count=0;
+}
+console.log(max_element);
